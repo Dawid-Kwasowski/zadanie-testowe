c2facabb6f69301ed90256cc56dffeaa06193d99
Dawid Kwasowski <dawidkwasowski@gmail.com>
2021-08-15 17:30:40 +0200

    add slide to top and bottom animation

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,9 +1,18 @@
-
-export const makeProgress = () => {
-      // symulation of loader / progress bar
-      const loaderFill = document.querySelector('.loader__fill')
-      loaderFill.style.width = loaderFill.getAttribute('aria-valuenow') + '%'
-      return setTimeout(() => {
-         // here it will be a functions SlideTop, SlideBottom .  new function name in progress 
-      },2000)
+const slideTo = direction => {
+      const slideEl = document.querySelector(`.slide-${direction}`)
+      slideEl.classList.add(`slide-${direction}--hide`)
+      console.log(`${direction} has new CSS class`)
 }
+// symulation of loader / progress bar
+export const makeProgress = () => {
+      const loaderFillEl = document.querySelector('.loader__fill')
+      // downloading max value from progress's attribute and assignnig this to width element 
+      loaderFillEl.style.width = loaderFillEl.getAttribute('aria-valuenow') + '%'
+      // then CSS transition in _intro.scss -> .loader -> &__fill make progressing effect 
+      return setTimeout(() => {
+      // calling function to add hidding CSS classes after 2 seconds + 100ms
+         slideTo('top')
+         slideTo('bottom')
+      },2100)
+}
+
