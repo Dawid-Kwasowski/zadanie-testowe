466a5b48bdf526d771832f80ed2cbda10b6cfa5d
Dawid Kwasowski <dawidkwasowski@gmail.com>
2021-08-17 18:39:50 +0200

    add player to bottom panel

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,15 @@
-import { makeProgress } from "./loader.js";
+import { makeProgress } from "./loader.js"
+import {playSong, volume} from "./player.js"
 
 addEventListener('DOMContentLoaded', ()=> {
    makeProgress()
+})
+
+const playBtn = document.querySelector('.player__icon--play-pause')
+const volumeBtn = document.querySelector('.player__icon--volume')
+playBtn.addEventListener('click', () => {
+   playSong(playBtn)
+})
+volumeBtn.addEventListener('click', () => {
+   volume(volumeBtn)
 })
---
+++ src/js/player.js
@@ -0,0 +1,24 @@
+let isPlaying = false
+let isMuted = false
+const playSong = (hook) => {
+   if(isPlaying) {
+     hook.src = '../src/images/pause-fill.svg' 
+   }
+   else {
+      hook.src = '../src/images/play-fill.svg'
+   }
+   isPlaying = !isPlaying 
+}
+
+const volume = (hook) => {
+   if(isMuted) {
+      hook.src = '../src/images/volume-up-fill.svg'
+   }
+   else {
+      hook.src = '../src/images/volume-mute-fill.svg'
+   }
+   isMuted = !isMuted
+}
+
+
+export {playSong, volume}
