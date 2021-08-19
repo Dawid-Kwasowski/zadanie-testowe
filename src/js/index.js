cc36df4c38335be979c6fadb2c0c6d11323d0753
Dawid Kwasowski <dawidkwasowski@gmail.com>
2021-08-19 19:57:15 +0200

    fix responsive issues

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,17 @@
-import { makeProgress } from "./loader.js"
+import { Loader } from "./loader.js"
 import {playSong, volume} from "./player.js"
 import {Maps} from "./map-box.js"
 
 const playBtn = document.querySelector('.player__icon--play-pause')
 const volumeBtn = document.querySelector('.player__icon--volume')
-
 addEventListener('DOMContentLoaded', () => {
-   makeProgress()
    const maps = new Maps
-   maps.createBoxes()
+   const loader = new Loader
+
+   loader.makeProgress()
+   setTimeout(() => {
+      maps.createBoxes()
+   },2100)
 })
 
 
--- src/js/loader.js
+++ src/js/loader.js
@@ -1,17 +1,24 @@
-const slideTo = direction => {
-      const slideEl = document.querySelector(`.slide-${direction}`)
-      slideEl.classList.add(`slide-${direction}--hide`)
-      // console.log(`${direction} has new CSS class`)
-}
-// symulation of loader / progress bar
-export const makeProgress = () => {
-      const loaderFillEl = document.querySelector('.loader__fill')
-      // downloading max value from progress's attribute and assignnig this to width element 
-      loaderFillEl.style.width = loaderFillEl.getAttribute('aria-valuenow') + '%'
-      // then CSS transition in _intro.scss -> .loader -> &__fill make progressing effect 
-      return setTimeout(() => {
-      // calling function to add hidding CSS classes after 2 seconds + 100ms
-         slideTo('top')
-         slideTo('bottom')
-      },2100)
+export class Loader {
+      // methods
+      slideTo(direction) {
+            const slideEl = document.querySelector(`.slide-${direction}`)
+            slideEl.classList.add(`slide-${direction}--hide`)
+            // console.log(`${direction} has new CSS class`)
+            // remove unnecessary element
+            return setTimeout(() => {
+                  slideEl.parentNode.removeChild(slideEl)
+            },500)
+      }
+      // symulation of loader / progress bar
+      makeProgress() {
+            const loaderFillEl = document.querySelector('.loader__fill')
+            // downloading max value from progress's attribute and assignnig this to width element 
+            loaderFillEl.style.width = loaderFillEl.getAttribute('aria-valuenow') + '%'
+            // then CSS transition in _intro.scss -> .loader -> &__fill make progressing effect 
+            return setTimeout(() => {
+            // calling function to add hidding CSS classes after 2 seconds + 100ms
+            this.slideTo('top')
+            this.slideTo('bottom')
+            },2100)
+      }
 }
