27735262305473c4fd6bfc79ee51f847f16fd82c
Dawid Kwasowski <dawidkwasowski@gmail.com>
2021-08-23 16:07:51 +0200

    add new features to music player

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -16,17 +16,10 @@ export const carrousel = gallery => $(gallery).slick({
        {
          breakpoint: 600,
          settings: {
-           slidesToShow: 2,
-           slidesToScroll: 2
-         }
-       },
-       {
-         breakpoint: 480,
-         settings: {
            slidesToShow: 1,
            slidesToScroll: 1
          }
-       }
+       },
    ]
 })
 
--- src/js/player.js
+++ src/js/player.js
@@ -1,12 +1,16 @@
-let isPlaying = false
+let isPlaying = true
 let isMuted = false
-
+const audio = document.querySelector('.player__audio')
 const playSong = (hook) => {
    if(isPlaying) {
      hook.src = '../src/images/pause-fill.svg'
+     audio.play()
+     setInterval(tracker,500)
    }
    else {
       hook.src = '../src/images/play-fill.svg'
+      audio.pause()
+      
    }
    isPlaying = !isPlaying 
 }
@@ -14,12 +18,24 @@ const playSong = (hook) => {
 const volume = (hook) => {
    if(isMuted) {
       hook.src = '../src/images/volume-up-fill.svg'
+      audio.muted = false
    }
    else {
       hook.src = '../src/images/volume-mute-fill.svg'
+      audio.muted = true
    }
    isMuted = !isMuted
 }
 
+const tracker = () => {
+   const progressBar = document.querySelector('.player__tracker-fill')
+   // value max = audio duration
+   progressBar.attributes[4].value = audio.duration
+   // current value = current time
+   progressBar.attributes[2].value = `${audio.currentTime}`
+   // width of the bar 
+   const convertedTime = Math.round(audio.currentTime)
+   progressBar.style.width = `${convertedTime / 2}%`
+}
 
 export {playSong, volume}
